fd452fcfb98ce6a79a2bdf5b8c84685da027e4cd
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-01 19:24:52 +0200

    Manage imports

--- ecg_classification/__init__.py
+++ ecg_classification/__init__.py
@@ -1,2 +1,4 @@
 # Import dataset class
 from ecg_classification.dataset import PhysioNetDataset
+# Import loss functions
+from ecg_classification.loss import SoftmaxFocalLoss, SoftmaxCrossEntropyLoss
