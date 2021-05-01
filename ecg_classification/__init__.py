0dba62441481172f9c48d793f3b611ca2d32f9d7
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-01 22:27:22 +0200

    Make different network sizes (S, M, L)

--- ecg_classification/__init__.py
+++ ecg_classification/__init__.py
@@ -5,4 +5,5 @@ from ecg_classification.loss import SoftmaxFocalLoss, SoftmaxCrossEntropyLoss
 # Import models
 from ecg_classification.model import ECGCNN, ECGAttNet
 # Import model configs
-from ecg_classification.config import ECGCNN_CONFIG, ECGAttNet_CONFIG
+from ecg_classification.config import ECGCNN_CONFIG_S, ECGAttNet_CONFIG_S, ECGCNN_CONFIG_M, ECGAttNet_CONFIG_M, \
+    ECGCNN_CONFIG_L, ECGAttNet_CONFIG_L
--- ecg_classification/config.py
+++ ecg_classification/config.py
@@ -1,18 +1,54 @@
 import torch.nn as nn
 
-# Configuration for ECGCNN
-ECGCNN_CONFIG = {
+# Configuration for ECGCNN S
+ECGCNN_CONFIG_S = {
+    "ecg_encoder_channels": ((80, 128), (128, 128), (128, 128), (128, 64), (64, 32)),
+    "spectrogram_encoder_channels": ((1, 16), (16, 16), (16, 32), (32, 64), (64, 256)),
+    "latent_vector_features": 256,
+    "classes": 4
+}
+
+# Configuration for ECGCNN M
+ECGCNN_CONFIG_M = {
     "ecg_encoder_channels": ((80, 128), (128, 256), (256, 256), (256, 128), (128, 32)),
     "spectrogram_encoder_channels": ((1, 16), (16, 32), (32, 64), (64, 128), (128, 256)),
     "latent_vector_features": 256,
     "classes": 4
 }
 
-# Configuration for ECGAttNet
-ECGAttNet_CONFIG = {
+# Configuration for ECGCNN L
+ECGCNN_CONFIG_L = {
+    "ecg_encoder_channels": ((80, 128), (128, 512), (512, 512), (512, 128), (128, 32)),
+    "spectrogram_encoder_channels": ((1, 32), (32, 64), (64, 128), (128, 256), (256, 256)),
+    "latent_vector_features": 256,
+    "classes": 4
+}
+
+# Configuration for ECGAttNet S
+ECGAttNet_CONFIG_S = {
+    "ecg_encoder_channels": ((80, 128), (128, 128), (128, 128), (128, 64), (64, 32)),
+    "ecg_encoder_spans": (256, 128, 64, 32, 16),
+    "spectrogram_encoder_channels": ((1, 16), (16, 16), (16, 32), (32, 64), (64, 256)),
+    "spectrogram_encoder_spans": (128, 64, 32, 16, 8),
+    "latent_vector_features": 256,
+    "classes": 4
+}
+
+# Configuration for ECGAttNet M
+ECGAttNet_CONFIG_M = {
     "ecg_encoder_channels": ((80, 128), (128, 256), (256, 256), (256, 128), (128, 32)),
     "ecg_encoder_spans": (256, 128, 64, 32, 16),
     "spectrogram_encoder_channels": ((1, 16), (16, 32), (32, 64), (64, 128), (128, 256)),
+    "spectrogram_encoder_spans": (128, 64, 32, 16, 8),
+    "latent_vector_features": 256,
+    "classes": 4
+}
+
+# Configuration for ECGAttNet L
+ECGAttNet_CONFIG_L = {
+    "ecg_encoder_channels": ((80, 128), (128, 512), (512, 512), (512, 128), (128, 32)),
+    "ecg_encoder_spans": (256, 128, 64, 32, 16),
+    "spectrogram_encoder_channels": ((1, 32), (32, 64), (64, 128), (128, 256), (256, 256)),
     "spectrogram_encoder_spans": (128, 64, 32, 16, 8),
     "latent_vector_features": 256,
     "classes": 4
