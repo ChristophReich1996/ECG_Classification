ea0073f8081c5ec9ff1379ea2d6670a6f9f5d736
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-02 00:05:43 +0200

    Manage imports

--- ecg_classification/__init__.py
+++ ecg_classification/__init__.py
@@ -9,3 +9,5 @@ from ecg_classification.config import ECGCNN_CONFIG_S, ECGAttNet_CONFIG_S, ECGCN
     ECGCNN_CONFIG_L, ECGAttNet_CONFIG_L, ECGInvNet_CONFIG_S, ECGInvNet_CONFIG_M, ECGInvNet_CONFIG_L
 # Import model wrapper
 from ecg_classification.model_wrapper import ModelWrapper
+# Import data logger
+from ecg_classification.logger import Logger
