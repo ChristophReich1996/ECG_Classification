790076389512d9cffb7067c00adfed9b4dd2768b
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-01 22:09:23 +0200

    Test ECGAttNet

--- train.py
+++ train.py
@@ -2,13 +2,13 @@ import torch
 
 from wettbewerb import load_references
 
-from ecg_classification import PhysioNetDataset, ECGCNN
+from ecg_classification import PhysioNetDataset, ECGCNN, ECGAttNet
 
 if __name__ == '__main__':
 
     ecg_leads, ecg_labels, fs, ecg_names = load_references("data/training/")
     dataset = PhysioNetDataset(ecg_leads=ecg_leads, ecg_labels=ecg_labels)
-    network = ECGCNN()
+    network = ECGAttNet()
 
     print(sum([p.numel() for p in network.parameters()]))
 
