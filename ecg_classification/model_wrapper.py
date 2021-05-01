d5f10d41131198cb0e3d7bb81249910908e0ab95
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-01 22:57:51 +0200

    Init model wrapper

---
+++ ecg_classification/model_wrapper.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+
+class ModelWrapper(object):
+    """
+    This class implements a wrapper for the training and validation loop.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def train(self) -> None:
+        pass
+
+    def validate(self) -> None:
+        pass
