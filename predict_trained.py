0f9391b198975df0e5b89961c81f86e43c52e171
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-01 16:51:20 +0200

    Add predict_trained.py from lecture repo

--- predict_trained.py
+++ predict_trained.py
@@ -1,1 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Diese Datei sollte nicht verändert werden und wird von uns gestellt und zurückgesetzt.
+Skript testet das neu trainierte Modell
+@author: Maurice Rohr
+"""
 
+from predict import predict_labels
+from wettbewerb import load_references, save_predictions
+
+if __name__ == '__main__':
+    ecg_leads, ecg_labels, fs, ecg_names = load_references(
+        '../test/')  # Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name                                                # Sampling-Frequenz 300 Hz
+
+    predictions = predict_labels("model.npy", ecg_leads, fs, ecg_names)
+
+    save_predictions(predictions)  # speichert Prädiktion in CSV Datei
