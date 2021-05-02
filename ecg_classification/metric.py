9fa6a55a2443e2bf9fb9bb578cedd156dd210111
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-02 11:03:12 +0200

    Implement F1 score

--- ecg_classification/metric.py
+++ ecg_classification/metric.py
@@ -24,7 +24,42 @@ class Accuracy(nn.Module):
         # Threshold prediction with argmax
         prediction_argmax = prediction.argmax(dim=-1)
         # Apply argmax to label
-        label = label.argmax(dim=-1)
+        label_argmax = label.argmax(dim=-1)
         # Compute accuracy
-        accuracy = (prediction_argmax == label).float().mean()
+        accuracy = (prediction_argmax == label_argmax).float().mean()
         return accuracy
+
+
+class F1(nn.Module):
+    """
+    This class implements the F1 score as a nn.Module
+    """
+
+    def __init__(self) -> None:
+        """
+        Constructor method
+        """
+        # Call super constructor
+        super(F1, self).__init__()
+
+    def forward(self, prediction: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass
+        :param prediction: (torch.Tensor) Prediction tensor with the shape [batch size, n classes]
+        :param label: (torch.Tensor) Label tensor with the shape [batch size, n classes]
+        :return: (torch.Tensor) Accuracy value
+        """
+        # Threshold prediction with max
+        prediction = (prediction == prediction.max(dim=-1, keepdim=True)[0]).float()
+        # Apply max to label
+        label = (label == label.max(dim=-1, keepdim=True)[0]).float()
+        # Calc tp, fp, fn
+        tp = (label * prediction).sum()
+        fp = ((1. - label) * prediction).sum()
+        fn = (label * (1. - prediction)).sum()
+        # Calc prediction and recall
+        precision = tp / (tp + fp + 1e-08)
+        recall = tp / (tp + fn + 1e-08)
+        # Calc F1 score
+        f1 = 2 * (precision * recall)  (precision + recall + 1e-08)
+        return f1
