2337748cb965d861293b99912936cd750bdd63c5
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-07 22:44:04 +0200

    Add script to estimate the dataset distribution

---
+++ scripts/dataset_distribution.py
@@ -0,0 +1,16 @@
+import numpy as np
+from ecg_classification import ECGDataset
+from wettbewerb import load_references
+
+if __name__ == '__main__':
+    # Init dataset
+    ecg_leads, ecg_labels, fs, ecg_names = load_references("../data/training/")
+    dataset = ECGDataset(ecg_leads, ecg_labels)
+    # Init distribution matrix
+    distribution = np.zeros(4, dtype=np.float32)
+    # Iterate over dataset
+    for _, _, labels in dataset:
+        # Make distribution
+        distribution[labels.argmax().numpy()] += 1.
+    print(distribution / distribution.sum())
+    print(1. - (distribution / distribution.sum()))
