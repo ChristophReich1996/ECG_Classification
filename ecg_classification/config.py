62961134034cfd3922e692cf3669a66abad87aca
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-08 00:21:14 +0200

    Roll back

--- ecg_classification/config.py
+++ ecg_classification/config.py
@@ -3,137 +3,119 @@ from involution import Involution2d
 
 # Configuration for ECGCNN S
 ECGCNN_CONFIG_S = {
+    "ecg_encoder_channels": ((80, 128), (128, 128), (128, 128), (128, 64), (64, 32)),
     "spectrogram_encoder_channels": ((1, 16), (16, 32), (32, 64), (64, 128), (128, 256)),
+    "latent_vector_features": 256,
     "classes": 4,
     "dropout": 0.05,
     "activation": nn.PReLU,
+    "convolution1d": nn.Conv1d,
     "convolution2d": nn.Conv2d,
-    "normalization2d": nn.BatchNorm2d,
-    "transformer_heads": 4,
-    "transformer_encoder_layers": 4,
-    "transformer_decoder_layers": 2,
-    "transformer_feedforward_dims": 256,
-    "transformer_activation": "gelu"
+    "normalization1d": nn.BatchNorm1d,
 }
 
 # Configuration for ECGCNN M
 ECGCNN_CONFIG_M = {
+    "ecg_encoder_channels": ((80, 128), (128, 256), (256, 256), (256, 128), (128, 32)),
     "spectrogram_encoder_channels": ((1, 32), (32, 64), (64, 128), (128, 256), (256, 256)),
+    "latent_vector_features": 256,
     "classes": 4,
     "dropout": 0.05,
     "activation": nn.PReLU,
+    "convolution1d": nn.Conv1d,
     "convolution2d": nn.Conv2d,
-    "normalization2d": nn.BatchNorm2d,
-    "transformer_heads": 4,
-    "transformer_encoder_layers": 5,
-    "transformer_decoder_layers": 3,
-    "transformer_feedforward_dims": 256,
-    "transformer_activation": "gelu"
+    "normalization1d": nn.BatchNorm1d,
 }
 
 # Configuration for ECGCNN L
 ECGCNN_CONFIG_L = {
+    "ecg_encoder_channels": ((80, 256), (256, 512), (512, 512), (512, 128), (128, 32)),
     "spectrogram_encoder_channels": ((1, 64), (64, 128), (128, 256), (256, 256), (256, 256)),
+    "latent_vector_features": 256,
     "classes": 4,
-    "dropout": 0.05,
+    "dropout": 0.0,
     "activation": nn.PReLU,
+    "convolution1d": nn.Conv1d,
     "convolution2d": nn.Conv2d,
-    "normalization2d": nn.BatchNorm2d,
-    "transformer_heads": 4,
-    "transformer_encoder_layers": 6,
-    "transformer_decoder_layers": 4,
-    "transformer_feedforward_dims": 256,
-    "transformer_activation": "gelu"
+    "normalization1d": nn.BatchNorm1d,
 }
 
 # Configuration for ECGAttNet S
 ECGAttNet_CONFIG_S = {
+    "ecg_encoder_channels": ((80, 128), (128, 128), (128, 128), (128, 64), (64, 32)),
+    "ecg_encoder_spans": (256, 128, 64, 32, 16),
     "spectrogram_encoder_channels": ((1, 16), (16, 32), (32, 64), (64, 128), (128, 256)),
     "spectrogram_encoder_spans": (128, 64, 32, 16, 8),
+    "latent_vector_features": 256,
     "classes": 4,
     "dropout": 0.05,
     "activation": nn.PReLU,
-    "normalization2d": nn.BatchNorm2d,
-    "transformer_heads": 4,
-    "transformer_encoder_layers": 4,
-    "transformer_decoder_layers": 2,
-    "transformer_feedforward_dims": 256,
-    "transformer_activation": "gelu"
+    "normalization1d": nn.BatchNorm1d,
 }
 
 # Configuration for ECGAttNet M
 ECGAttNet_CONFIG_M = {
+    "ecg_encoder_channels": ((80, 128), (128, 256), (256, 256), (256, 128), (128, 32)),
+    "ecg_encoder_spans": (256, 128, 64, 32, 16),
     "spectrogram_encoder_channels": ((1, 32), (32, 64), (64, 128), (128, 256), (256, 256)),
     "spectrogram_encoder_spans": (128, 64, 32, 16, 8),
+    "latent_vector_features": 256,
     "classes": 4,
     "dropout": 0.05,
     "activation": nn.PReLU,
-    "normalization2d": nn.BatchNorm2d,
-    "transformer_heads": 4,
-    "transformer_encoder_layers": 5,
-    "transformer_decoder_layers": 3,
-    "transformer_feedforward_dims": 256,
-    "transformer_activation": "gelu"
+    "normalization1d": nn.BatchNorm1d,
 }
 
 # Configuration for ECGAttNet L
 ECGAttNet_CONFIG_L = {
+    "ecg_encoder_channels": ((80, 256), (256, 512), (512, 512), (512, 128), (128, 32)),
+    "ecg_encoder_spans": (256, 128, 64, 32, 16),
     "spectrogram_encoder_channels": ((1, 64), (64, 128), (128, 256), (256, 256), (256, 256)),
     "spectrogram_encoder_spans": (128, 64, 32, 16, 8),
+    "latent_vector_features": 256,
     "classes": 4,
     "dropout": 0.05,
     "activation": nn.PReLU,
-    "normalization2d": nn.BatchNorm2d,
-    "transformer_heads": 4,
-    "transformer_encoder_layers": 6,
-    "transformer_decoder_layers": 4,
-    "transformer_feedforward_dims": 256,
-    "transformer_activation": "gelu"
+    "normalization1d": nn.BatchNorm1d,
 }
 
 # Configuration for ECGInvNet S
 ECGInvNet_CONFIG_S = {
-    "spectrogram_encoder_channels": ((1, 64), (64, 128), (128, 256), (256, 256), (256, 256)),
+    "ecg_encoder_channels": ((80, 128), (128, 128), (128, 128), (128, 64), (64, 32)),
+    "ecg_encoder_spans": (256, 128, 64, 32, 16),
+    "spectrogram_encoder_channels": ((1, 16), (16, 32), (32, 64), (64, 128), (128, 256)),
+    "latent_vector_features": 256,
     "classes": 4,
     "dropout": 0.05,
     "activation": nn.PReLU,
     "convolution2d": Involution2d,
-    "normalization2d": nn.BatchNorm2d,
-    "transformer_heads": 4,
-    "transformer_encoder_layers": 4,
-    "transformer_decoder_layers": 2,
-    "transformer_feedforward_dims": 256,
-    "transformer_activation": "gelu"
+    "normalization1d": nn.BatchNorm1d,
 }
 
 # Configuration for ECGInvNet M
 ECGInvNet_CONFIG_M = {
+    "ecg_encoder_channels": ((80, 128), (128, 256), (256, 256), (256, 128), (128, 32)),
+    "ecg_encoder_spans": (256, 128, 64, 32, 16),
     "spectrogram_encoder_channels": ((1, 32), (32, 64), (64, 128), (128, 256), (256, 256)),
+    "latent_vector_features": 256,
     "classes": 4,
     "dropout": 0.05,
     "activation": nn.PReLU,
     "convolution2d": Involution2d,
-    "normalization2d": nn.BatchNorm2d,
-    "transformer_heads": 4,
-    "transformer_encoder_layers": 5,
-    "transformer_decoder_layers": 3,
-    "transformer_feedforward_dims": 256,
-    "transformer_activation": "gelu"
+    "normalization1d": nn.BatchNorm1d,
 }
 
 # Configuration for ECGInvNet L
 ECGInvNet_CONFIG_L = {
+    "ecg_encoder_channels": ((80, 256), (256, 512), (512, 512), (512, 128), (128, 32)),
+    "ecg_encoder_spans": (256, 128, 64, 32, 16),
     "spectrogram_encoder_channels": ((1, 64), (64, 128), (128, 256), (256, 256), (256, 256)),
+    "latent_vector_features": 256,
     "classes": 4,
     "dropout": 0.05,
     "activation": nn.PReLU,
     "convolution2d": Involution2d,
-    "normalization2d": nn.BatchNorm2d,
-    "transformer_heads": 4,
-    "transformer_encoder_layers": 6,
-    "transformer_decoder_layers": 4,
-    "transformer_feedforward_dims": 256,
-    "transformer_activation": "gelu"
+    "normalization1d": nn.BatchNorm1d,
 }
 
 # Configuration of augmentation pipeline
