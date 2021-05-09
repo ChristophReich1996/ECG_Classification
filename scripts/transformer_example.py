674251c7219040feb324f066389f663253c2481c
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-09 12:30:14 +0200

    Update scripts

--- scripts/transformer_example.py
+++ scripts/transformer_example.py
@@ -2,13 +2,12 @@ import torch
 import torch.nn as nn
 
 if __name__ == '__main__':
-    transformer = nn.Transformer(d_model=128, nhead=2, num_encoder_layers=2, num_decoder_layers=2,
-                                 dim_feedforward=256, dropout=0.1, activation='relu', custom_encoder=None,
+    transformer = nn.Transformer(d_model=500, nhead=2, num_encoder_layers=2, num_decoder_layers=2,
+                                 dim_feedforward=500, dropout=0.1, activation='relu', custom_encoder=None,
                                  custom_decoder=None)
-    src_1 = torch.rand(12, 128)
-    src_2 = torch.rand(8, 128)
-    src = torch.nn.utils.rnn.pad_packed_sequence(torch.nn.utils.rnn.pack_sequence([src_1, src_2]), batch_first=False)[0]
-    tgt = torch.rand(4, 2, 128)
+    transformer.to("cuda:1")
+    src = torch.rand(18000 // 4, 1, 500).to("cuda:1")
+    tgt = torch.rand(4, 1, 500).to("cuda:1")
     output = transformer(src, tgt)
     print(output.shape)
 
--- scripts/wavelet_example.py
+++ scripts/wavelet_example.py
@@ -2,20 +2,14 @@ import pywt
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+import torchaudio
 
 from wettbewerb import load_references
 
 if __name__ == '__main__':
     ecg_leads = load_references("../data/training/")[0]
-    s, a = pywt.cwt(ecg_leads[0], range(1, 129), 'morl', 1)
-    print(s.shape, ecg_leads[0].shape)
-    plt.imshow(s[:, :127], aspect="auto")
-    plt.colorbar()
-    plt.show()
-    print(torch.from_numpy(s).unfold(dimension=-1, size=128, step=128).shape)
-
-    s, a = pywt.cwt(ecg_leads[0][:3000], range(1, 129), 'morl', 1)
-    print(s.shape, ecg_leads[0][:3000].shape)
-    plt.imshow(s[:, :127], aspect="auto")
+    s, a = pywt.cwt(ecg_leads[0].astype(float), range(1, 501), wavelet="gaus1")
+    print(s.shape, ecg_leads[0].shape, a.shape)
+    plt.imshow(s[:, :1000], aspect="auto")
     plt.colorbar()
     plt.show()
