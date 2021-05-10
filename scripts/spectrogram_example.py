95a446e071ac6c2a303b1e1de9c07af3a0208623
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-10 19:14:49 +0200

    Update spectrogram example script

--- scripts/spectrogram_example.py
+++ scripts/spectrogram_example.py
@@ -8,8 +8,12 @@ from wettbewerb import load_references
 
 if __name__ == '__main__':
     ecg_leads = load_references("../data/training/")[0]
-    ecg_lead = torch.from_numpy(ecg_leads[0]).float()
-    s = torchaudio.transforms.Spectrogram(n_fft=64, win_length=64, hop_length=32, power=2, normalized=True)(ecg_lead)
+    for ecg_lead_ in ecg_leads:
+        if ecg_lead_.shape[0] == 18000:
+            ecg_lead = torch.from_numpy(ecg_lead_).float()
+            break
+    print(ecg_lead.shape)
+    s = torchaudio.transforms.Spectrogram(n_fft=64, win_length=64, hop_length=32, power=2, normalized=False)(ecg_lead)
     s = torch.log(s.clamp(min=1e-08))
     print(s.shape)
     plt.imshow(s, aspect="auto")
