6aa75652804f517cf9571f9d724997b2ee33c5cf
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-11 20:39:00 +0200

    Debug script to run experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,7 +1,7 @@
-python -W ingore train.py --cuda_devices "0" --epochs 100 -- batch_size 24 --physio_net --dataset_path "data/training2017" --network_config "ECGCNN_S"
-python -W ingore train.py --cuda_devices "0" --epochs 100 -- batch_size 24 --physio_net --dataset_path "data/training2017" --network_config "ECGCNN_M"
-python -W ingore train.py --cuda_devices "0" --epochs 100 -- batch_size 24 --physio_net --dataset_path "data/training2017" --network_config "ECGCNN_L"
-python -W ingore train.py --cuda_devices "0" --epochs 100 -- batch_size 24 --physio_net --dataset_path "data/training2017" --network_config "ECGAttNet_S"
-python -W ingore train.py --cuda_devices "0" --epochs 100 -- batch_size 24 --physio_net --dataset_path "data/training2017" --network_config "ECGAttNet_M"
-python -W ingore train.py --cuda_devices "0" --epochs 100 -- batch_size 24 --physio_net --dataset_path "data/training2017" --network_config "ECGAttNet_L"
+python -W ignore train.py --cuda_devices "0" --epochs 100 --batch_size 24 --physio_net --dataset_path "data/training2017/" --network_config "ECGCNN_S"
+python -W ignore train.py --cuda_devices "0" --epochs 100 --batch_size 24 --physio_net --dataset_path "data/training2017/" --network_config "ECGCNN_M"
+python -W ignore train.py --cuda_devices "0" --epochs 100 --batch_size 24 --physio_net --dataset_path "data/training2017/" --network_config "ECGCNN_L"
+python -W ignore train.py --cuda_devices "0" --epochs 100 --batch_size 24 --physio_net --dataset_path "data/training2017/" --network_config "ECGAttNet_S"
+python -W ignore train.py --cuda_devices "0" --epochs 100 --batch_size 24 --physio_net --dataset_path "data/training2017/" --network_config "ECGAttNet_M"
+python -W ignore train.py --cuda_devices "0" --epochs 100 --batch_size 24 --physio_net --dataset_path "data/training2017/" --network_config "ECGAttNet_L"
 
