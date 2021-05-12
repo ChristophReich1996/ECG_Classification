0335270a3425f93db38e03085e150b765d059a3a
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-12 12:44:46 +0200

    Print more decimal places

--- scripts/get_results_for_logs.py
+++ scripts/get_results_for_logs.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
         print(folder)
         acc = torch.load(os.path.join("../experiments", folder, "metrics", "Accuracy.pt"))
         f1 = torch.load(os.path.join("../experiments", folder, "metrics", "F1.pt"))
-        print("ACC:", acc.max())
-        print("F1:", f1[acc.argmax()])
+        print("ACC:", acc.max().item())
+        print("F1:", f1[acc.argmax()].item())
