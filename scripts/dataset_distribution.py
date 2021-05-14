a26574b68a0d80840161909eca05fb277fdef6ad
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-15 00:30:14 +0200

    Print raw distribution

--- scripts/dataset_distribution.py
+++ scripts/dataset_distribution.py
@@ -4,7 +4,7 @@ from wettbewerb import load_references
 
 if __name__ == '__main__':
     # Init dataset
-    ecg_leads, ecg_labels, fs, ecg_names = load_references("../data/training/")
+    ecg_leads, ecg_labels, fs, ecg_names = load_references("../data/training2017/")
     dataset = ECGDataset(ecg_leads, ecg_labels)
     # Init distribution matrix
     distribution = np.zeros(4, dtype=np.float32)
@@ -12,5 +12,6 @@ if __name__ == '__main__':
     for _, _, labels in dataset:
         # Make distribution
         distribution[labels.argmax().numpy()] += 1.
+    print(distribution)
     print(distribution / distribution.sum())
     print(1. - (distribution / distribution.sum()))
