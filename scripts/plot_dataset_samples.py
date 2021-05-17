98214cbf419dcc8d74cf0ea24c931ec9659faeb6
VladiKruk <vladi-kruk@gmx.de>
2021-05-17 15:19:19 +0200

    Add plot function for data samples Add plot.tex

--- scripts/plot_dataset_samples.py
+++ scripts/plot_dataset_samples.py
@@ -6,7 +6,9 @@ from wettbewerb import load_references
 
 if __name__ == '__main__':
     # Get data
-    ecg_leads, ecg_labels, fs, ecg_names = load_references("../data/training/")
+    ecg_leads, ecg_labels, fs, ecg_names = load_references("D:/ECG_Classification Data/training/")
     # Plot a few data samples
-    for ecg_lead, ecg_label in zip(ecg_labels, ecg_labels):
+    for ecg_lead, ecg_label in zip(ecg_leads, ecg_labels):
+        plt.plot(ecg_lead)
+        tikzplotlib.save("plot.tex")
         pass
