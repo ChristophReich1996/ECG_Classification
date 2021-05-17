368ca6fc5d941520100851ebe6b443c71ba068ca
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-17 22:18:14 +0200

    Debug and clean up plotting

--- scripts/plot_dataset_samples.py
+++ scripts/plot_dataset_samples.py
@@ -1,20 +1,24 @@
 import matplotlib.pyplot as plt
 import tikzplotlib
+import numpy as np
 
 from wettbewerb import load_references
 
-
 if __name__ == '__main__':
     # Get data
-    ecg_leads, ecg_labels, fs, ecg_names = load_references("D:/ECG_Classification Data/training/")
+    ecg_leads, ecg_labels, fs, ecg_names = load_references("../data/training/")
+    # Init dict for plotting samples from all classes
+    plot_dict = {"N": False, "O": False, "A": False, "~": False}
+    # Set sample length
+    sample_length = 1250
     # Plot a few data samples
-    for ecg_lead, ecg_label in zip(ecg_leads, ecg_labels):
-        plt.xlim(0, 1000)
-        plt.ylim(-150, 300)
-        plt.xlabel('time (ms)')
-        plt.ylabel('voltage (mV)')
-        plt.plot(ecg_lead)
-
-
-        #tikzplotlib.save("plot_raw.tex")
-        pass
+    for index, (ecg_lead, ecg_label) in enumerate(zip(ecg_leads, ecg_labels)):
+        if not plot_dict[ecg_label]:
+            plot_dict[ecg_label] = True
+            x = np.arange(0, sample_length, dtype=np.float32) / fs
+            plt.plot(x, ecg_lead[-sample_length:])
+            plt.xlabel("time/s")
+            plt.ylabel("voltage/mV")
+            plt.title(ecg_label)
+            tikzplotlib.save("{}_{}.tex".format(index, ecg_label), axis_width="\\figW", axis_height="\\figH")
+            plt.show()
