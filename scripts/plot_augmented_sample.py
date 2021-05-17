194425cc48dbd2665a4d395991b9c1ca46fc3c89
VladiKruk <vladi-kruk@gmx.de>
2021-05-17 16:58:47 +0200

    Removed line

--- scripts/plot_augmented_sample.py
+++ scripts/plot_augmented_sample.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import tikzplotlib
 
 from wettbewerb import load_references
+from ecg_classification import augmentation
 
 
 if __name__ == '__main__':
@@ -9,12 +10,16 @@ if __name__ == '__main__':
     ecg_leads, ecg_labels, fs, ecg_names = load_references("D:/ECG_Classification Data/training/")
     # Plot a few data samples
     for ecg_lead, ecg_label in zip(ecg_leads, ecg_labels):
+        # Limit x- and y-axis of plot
         plt.xlim(0, 1000)
         plt.ylim(-150, 300)
+
+        # Label x- and y-axis of plot
         plt.xlabel('time (ms)')
         plt.ylabel('voltage (mV)')
+        plt.axis()
         plt.plot(ecg_lead)
-
+        plt.show()
 
         #tikzplotlib.save("plot_raw.tex")
         pass
