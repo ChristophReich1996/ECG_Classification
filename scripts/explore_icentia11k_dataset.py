dab987e5d52dcf8590255555ae913bece8e9d627
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-18 00:42:02 +0200

    Explore labels

--- scripts/explore_icentia11k_dataset.py
+++ scripts/explore_icentia11k_dataset.py
@@ -1,5 +1,6 @@
 import gzip
 import pickle
+import os
 
 if __name__ == '__main__':
     # Load sample
@@ -9,5 +10,16 @@ if __name__ == '__main__':
 
     # Load label
     with gzip.open("E:\\ECG_Data\\icentia11k\\00000_batched_lbls.pkl.gz", "rb") as file:
-        data = pickle.load(file)
-    print(data)
+        lbls = pickle.load(file)
+    print(lbls)
+
+    classes = []
+    for index, file in enumerate(os.listdir("E:\\ECG_Data\\icentia11k")):
+        print(index)
+        if "lbls" in file:
+            with gzip.open(os.path.join("E:\\ECG_Data\\icentia11k", file), "rb") as file:
+                lbls = pickle.load(file)
+            for lbl in lbls:
+                classes.append(sum([label.shape[0] != 0 for label in lbl["rtype"]]))
+        if index > 1000:
+            exit(22)
