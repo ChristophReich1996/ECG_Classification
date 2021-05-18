5095cf8b45439bf2e2edc185d018bf919395975c
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-05-18 15:01:22 +0200

    Catch case if project is incomplete

--- scripts/get_results_for_logs.py
+++ scripts/get_results_for_logs.py
@@ -4,7 +4,10 @@ import os
 if __name__ == '__main__':
     for folder in os.listdir(path="../experiments"):
         print(folder)
-        acc = torch.load(os.path.join("../experiments", folder, "metrics", "Accuracy.pt"))
-        f1 = torch.load(os.path.join("../experiments", folder, "metrics", "F1.pt"))
-        print("ACC:", acc.max().item())
-        print("F1:", f1[acc.argmax()].item())
+        try:
+            acc = torch.load(os.path.join("../experiments", folder, "metrics", "Accuracy.pt"))
+            f1 = torch.load(os.path.join("../experiments", folder, "metrics", "F1.pt"))
+            print("ACC:", acc.max().item())
+            print("F1:", f1[acc.argmax()].item())
+        except:
+            pass
