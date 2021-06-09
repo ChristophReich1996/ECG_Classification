c04cd0bf47f0cce27fd5b314011f0f887a9efd1b
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-06-09 16:18:48 +0200

    Add new code from 18-ha-2010-pj repo

--- predict_pretrained.py
+++ predict_pretrained.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Diese Datei sollte nicht verändert werden und wird von uns gestellt und zurückgesetzt.
-Skript testet das neu trainierte Modell
+Skript testet das vortrainierte Modell
 @author: Maurice Rohr
 """
 
@@ -12,6 +12,6 @@ if __name__ == '__main__':
     ecg_leads, ecg_labels, fs, ecg_names = load_references(
         '../test/')  # Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name                                                # Sampling-Frequenz 300 Hz
 
-    predictions = predict_labels(ecg_leads, fs, ecg_names, use_pretrained=False)
+    predictions = predict_labels(ecg_leads, fs, ecg_names, use_pretrained=True)
 
     save_predictions(predictions)  # speichert Prädiktion in CSV Datei
