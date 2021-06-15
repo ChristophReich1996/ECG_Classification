d53ba479740f74d2f8b0113618c951dec937e678
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-06-15 17:28:55 +0200

    Add structure

--- predict.py
+++ predict.py
@@ -3,9 +3,13 @@
 This script tests the trained model
 Authors: Christoph Hoog Antink, Maurice Rohr and Christoph Reich
 """
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
 import numpy as np
+import os
 
 
 def predict_labels(ecg_leads: List[np.ndarray], fs: float, ecg_names: List[str],
@@ -17,6 +21,32 @@ def predict_labels(ecg_leads: List[np.ndarray], fs: float, ecg_names: List[str],
     :param ecg_names: (List[str]) List of strings with name of each ecg lead
     :param use_pretrained: (bool) If true pre-trained (trained!) model is used
     :param two_classes: (bool) If true model for two classes is utilized else four class model is used
+    :return: (List[Tuple[str, str]]) List of tuples including name and prediction
+    """
+    # Train model if utilized
+    if not use_pretrained:
+        pass
+    # Load model
+    else:
+        pass
+
+
+def _train(model: Union[nn.Module, nn.DataParallel]) -> Union[nn.Module, nn.DataParallel]:
+    """
+    Private function which trains the given model
+    :param model: (Union[nn.Module, nn.DataParallel]) Model to be trained
+    :return: (Union[nn.Module, nn.DataParallel]) Trained model
+    """
+    pass
+
+
+def _predict(model: Union[nn.Module, nn.DataParallel], dataset: DataLoader,
+             ecg_names: List[str]) -> List[Tuple[str, str]]:
+    """
+    Private function to make predictions
+    :param model: (Union[nn.Module, nn.DataParallel]) Trained model
+    :param dataset: (DataLoader) Dataset to be predicted
+    :param ecg_names: (List[str]) Name of each sample
     :return: (List[Tuple[str, str]]) List of tuples including name and prediction
     """
     pass
