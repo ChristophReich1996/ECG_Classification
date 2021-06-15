ac1c7e9c59d57ecd4556eb0b5e7981e866391418
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-06-15 16:55:03 +0200

    Clean up

--- predict.py
+++ predict.py
@@ -1,57 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Skript testet das vortrainierte Modell
-@author: Christoph Hoog Antink, Maurice Rohr
+This script tests the trained model
+Authors: Christoph Hoog Antink, Maurice Rohr and Christoph Reich
 """
+from typing import List, Tuple
 
-import csv
-import scipy.io as sio
-import matplotlib.pyplot as plt
 import numpy as np
-from ecgdetectors import Detectors
-import os
 
 
-###Signatur der Methode (Parameter und Anzahl return-Werte) darf nicht verändert werden
-def predict_labels(ecg_leads, fs, ecg_names, use_pretrained=False):
-    '''
-    Parameters
-    ----------
-    model_name : str
-        Dateiname des Models. In Code-Pfad
-    ecg_leads : list of numpy-Arrays
-        EKG-Signale.
-    fs : float
-        Sampling-Frequenz der Signale.
-    ecg_names : list of str
-        eindeutige Bezeichnung für jedes EKG-Signal.
-    Returns
-    -------
-    predictions : list of tuples
-        ecg_name und eure Diagnose
-    '''
-
-    # ------------------------------------------------------------------------------
-    # Euer Code ab hier
-    model_name = "model.npy"
-    if use_pretrained:
-        model_name = "model_pretrained.npy"
-    with open(model_name, 'rb') as f:
-        th_opt = np.load(f)  # Lade simples Model (1 Parameter)
-
-    detectors = Detectors(fs)  # Initialisierung des QRS-Detektors
-
-    predictions = list()
-
-    for idx, ecg_lead in enumerate(ecg_leads):
-        r_peaks = detectors.hamilton_detector(ecg_lead)  # Detektion der QRS-Komplexe
-        sdnn = np.std(np.diff(r_peaks) / fs * 1000)
-        if sdnn < th_opt:
-            predictions.append((ecg_names[idx], 'N'))
-        else:
-            predictions.append((ecg_names[idx], 'A'))
-        if ((idx + 1) % 100) == 0:
-            print(str(idx + 1) + "\t Dateien wurden verarbeitet.")
-
-    # ------------------------------------------------------------------------------
-    return predictions  # Liste von Tupels im Format (ecg_name,label) - Muss unverändert bleiben!
+def predict_labels(ecg_leads: List[np.ndarray], fs: float, ecg_names: List[str],
+                   use_pretrained: bool = False, two_classes: bool = True) -> List[Tuple[str, str]]:
+    """
+    Function to produce predictions
+    :param ecg_leads: (List[np.ndarray]) ECG leads as a list of numpy arrays
+    :param fs: (float) Sampling frequency
+    :param ecg_names: (List[str]) List of strings with name of each ecg lead
+    :param use_pretrained: (bool) If true pre-trained (trained!) model is used
+    :param two_classes: (bool) If true model for two classes is utilized else four class model is used
+    :return: (List[Tuple[str, str]]) List of tuples including name and prediction
+    """
+    pass
