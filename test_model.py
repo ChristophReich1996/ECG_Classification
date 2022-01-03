8e2e48c22699af265ca2222dea27b5ec4034e997
MauriceRohr <34025664+MauriceRohr@users.noreply.github.com>
2022-01-03 18:42:34 +0100

    Update test_model.py
    USE PRETRAINED = TRUE

--- test_model.py
+++ test_model.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     ecg_leads,ecg_labels,fs,ecg_names = load_references(args.data_directory) # Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name                                                # Sampling-Frequenz 300 Hz
 
     start_time = time.time()
-    predictions = predict_labels(ecg_leads,fs,ecg_names, is_binary_classifier=False, return_probability=True)
+    predictions = predict_labels(ecg_leads,fs,ecg_names,use_pretrained=True, is_binary_classifier=False, return_probability=True)
     pred_time = time.time()-start_time
 
     save_predictions(predictions,folder=args.output_directory) # speichert Prädiktion in CSV Datei
