7efafa5ef60b4ae2594ecbd438b9d5970c68c8b1
ChristophReich1996 <christophreich@gmx.net>
2023-03-29 21:41:54 +0200

    Update scripts

--- scripts/explore_icentia11k_dataset.py
+++ scripts/explore_icentia11k_dataset.py
@@ -4,24 +4,30 @@ import os
 
 if __name__ == '__main__':
     # Load sample
-    with gzip.open("E:\\ECG_Data\\icentia11k\\00000_batched.pkl.gz", "rb") as file:
+    with gzip.open("/mnt/scratch/creich/icentia11k/00000_batched.pkl.gz", "rb") as file:
         data = pickle.load(file)
     print(data.shape)
 
     # Load label
-    with gzip.open("E:\\ECG_Data\\icentia11k\\00000_batched_lbls.pkl.gz", "rb") as file:
+    with gzip.open("/mnt/scratch/creich/icentia11k/00200_batched_lbls.pkl.gz", "rb") as file:
         lbls = pickle.load(file)
     print(lbls)
 
+
     classes = []
-    for index, file in enumerate(os.listdir("E:\\ECG_Data\\icentia11k")):
-        print(index)
+    noise = 0
+    nsr = 0
+    afib = 0
+    afl = 0
+    for index, file in enumerate(os.listdir("/mnt/scratch/creich/icentia11k")):
         if "lbls" in file:
-            with gzip.open(os.path.join("E:\\ECG_Data\\icentia11k", file), "rb") as file:
+            with gzip.open(os.path.join("/mnt/scratch/creich/icentia11k", file), "rb") as file:
                 lbls = pickle.load(file)
-            for lbl in lbls:
-                classes.append(sum([label.shape[0] != 0 for label in lbl["rtype"]]))
-                if classes[-1] >= 3:
-                    print("Stop")
-        if index > 1000:
-            exit(22)
+                for sample in lbls:
+                    rtype = sample["rtype"]
+                    noise += (len(rtype[2]) + len(rtype[1]) + len(rtype[0]))
+                    nsr += len(rtype[3])
+                    afib += len(rtype[4])
+                    afl += len(rtype[5])
+        print(index)
+    print(noise, nsr, afib, afl)
--- scripts/icentia11k_dataset_distribution.py
+++ scripts/icentia11k_dataset_distribution.py
@@ -5,15 +5,17 @@ from ecg_classification import Icentia11kDataset, icentia11k_dataset_collate_fn
 if __name__ == '__main__':
     # Init dataset
     dataset = DataLoader(Icentia11kDataset(path="/home/creich/scratch/icentia11k", split=list(range(11000))),
-                         num_workers=12, collate_fn=icentia11k_dataset_collate_fn, batch_size=1)
+                         num_workers=0, collate_fn=icentia11k_dataset_collate_fn, batch_size=1)
     # Init distribution matrix
-    distribution = np.zeros(7, dtype=np.float32)
+    distribution = np.zeros(4, dtype=np.float32)
     # Iterate over dataset
     for index, (ecg_lead, spectrogram, labels) in enumerate(dataset):
         print(index, ecg_lead.shape, spectrogram.shape)
         # Make distribution
         for label in labels:
             distribution[label.argmax().numpy()] += 1.
+        if index > 100:
+            break
     print(distribution)
     print(distribution / distribution.sum())
     print(1. - (distribution / distribution.sum()))
